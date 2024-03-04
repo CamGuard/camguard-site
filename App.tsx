54be6b53136dae202bcc03d80dd9b6b433acea56
Leg3ndary <ben.zhou.2006@gmail.com>
2024-03-04 15:56:01 -0500

    Done

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { IoIosArrowBack, IoIosArrowForward, IoIosHome } from "react-icons/io";
+import { RiCameraLensFill } from "react-icons/ri";
 
 export default function App() {
     const { width, height } = Dimensions.get("window");
@@ -8,10 +10,13 @@ export default function App() {
         <View style={styles.container}>
             <StatusBar style="auto" />
             <View style={[styles.header, { width: width }]}>
-                <Text>Header</Text>
+                <RiCameraLensFill style={styles.bigLogo} />
+                <Text style={styles.logo}>CamGuard</Text>
             </View>
             <View style={[styles.footer, { width: width }]}>
-                <Text>Open up App.tsx to start working on your app!</Text>
+                <IoIosArrowBack style={styles.icon} />
+                <IoIosHome style={styles.icon} />
+                <IoIosArrowForward style={styles.icon} />
             </View>
         </View>
     );
@@ -23,22 +28,41 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         alignItems: "center",
     },
+    bigLogo: {
+        color: "#007AFF",
+        fontSize: 40,
+        marginHorizontal: 4,
+    },
+    logo: {
+        fontSize: 25,
+        fontWeight: "900",
+        marginHorizontal: 8,
+    },
     footer: {
         position: "absolute",
         bottom: 0,
         backgroundColor: "#f0f0f0",
         height: 75,
         display: "flex",
-        justifyContent: "center",
+        justifyContent: "space-evenly",
         alignItems: "center",
+        flexDirection: "row",
     },
     header: {
         position: "relative",
         top: 0,
         backgroundColor: "#fff",
         display: "flex",
-        justifyContent: "center",
+        flexDirection: "row",
         alignItems: "center",
         height: 75,
+        borderBottomColor: "#D9D9D9",
+        borderStyle: "solid",
+        borderBottomWidth: 2,
+        paddingHorizontal: 15,
+    },
+    icon: {
+        color: "#007AFF",
+        fontSize: 40,
     },
 });
