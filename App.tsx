f06e14e912f4fa8963163e5180e8d509a03f917f
Leg3ndary <ben.zhou.2006@gmail.com>
2024-03-04 15:36:34 -0500

    Push react icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 export default function App() {
+    const { width, height } = Dimensions.get("window");
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
             <StatusBar style="auto" />
+            <View style={[styles.header, { width: width }]}>
+                <Text>Header</Text>
+            </View>
+            <View style={[styles.footer, { width: width }]}>
+                <Text>Open up App.tsx to start working on your app!</Text>
+            </View>
         </View>
     );
 }
@@ -15,6 +22,23 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
+    },
+    footer: {
+        position: "absolute",
+        bottom: 0,
+        backgroundColor: "#f0f0f0",
+        height: 75,
+        display: "flex",
         justifyContent: "center",
+        alignItems: "center",
+    },
+    header: {
+        position: "relative",
+        top: 0,
+        backgroundColor: "#fff",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 75,
     },
 });
