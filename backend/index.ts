a6d5dd25102ca298d87f76bfbb0c60c6982b33e1
Leg3ndary <ben.zhou.2006@gmail.com>
2024-04-22 15:05:17 -0400

    lint

--- backend/index.ts
+++ backend/index.ts
@@ -1,38 +1,38 @@
-import * as express from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as sharp from 'sharp';
+import * as express from "express";
+import * as fs from "fs";
+import * as path from "path";
+import * as sharp from "sharp";
 
 const app = express();
 const port = 3000;
-const imageFolder = 'backend/resources';
+const imageFolder = "backend/resources";
 
-app.get('/mjpg', (req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'multipart/x-mixed-replace; boundary=frame',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-    'Pragma': 'no-cache',
-  });
+app.get("/mjpg", (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "multipart/x-mixed-replace; boundary=frame",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+        Pragma: "no-cache",
+    });
 
-  const sendImage = async () => {
-    const files = fs.readdirSync(imageFolder).slice(1);
-    for (const file of files) {
-      const imagePath = path.join(imageFolder, file);
-      const imageBuffer = await sharp(imagePath).jpeg().toBuffer();
+    const sendImage = async () => {
+        const files = fs.readdirSync(imageFolder).slice(1);
+        for (const file of files) {
+            const imagePath = path.join(imageFolder, file);
+            const imageBuffer = await sharp(imagePath).jpeg().toBuffer();
 
-      res.write(`--frame\r\n`);
-      res.write(`Content-Type: image/jpeg\r\n`);
-      res.write(`Content-Length: ${imageBuffer.length}\r\n\r\n`);
-      res.write(imageBuffer);
-      res.write('\r\n');
-    }
-    setTimeout(sendImage, 100);
-  };
+            res.write(`--frame\r\n`);
+            res.write(`Content-Type: image/jpeg\r\n`);
+            res.write(`Content-Length: ${imageBuffer.length}\r\n\r\n`);
+            res.write(imageBuffer);
+            res.write("\r\n");
+        }
+        setTimeout(sendImage, 100);
+    };
 
-  sendImage();
+    sendImage();
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
